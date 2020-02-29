449b0c938f31d96ed00408a500e43656093da9a9
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-03-01 03:34:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python3
 import sys
-from PySide2 import QtCore, QtWidgets, QtGui
-from src_dir.frontend import MyWidget
+from tkinter import *
+from tkinter.ttk import *
+from src_dir.frontend import Application
 import src_dir.backend
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    root = Tk()
+    root.title("Bookstore Management System")
+    root.geometry("500x450")
+    # preventing a resize
+    # root.resizable(False, False)
 
-    widget = MyWidget()
-    widget.show()
 
-    sys.exit(app.exec_())
+    # instantiate the class
+    app = Application(master=root)
+    # loop it
+    app.mainloop()
+    # destroy it once you
+    # are done
+    root.destroy()
