4d095b9f3a070d07738462edfa493e594ae03d3c
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-02-29 18:40:03 +0600

    Update frontend.py

--- src_dir/frontend.py
+++ src_dir/frontend.py
@@ -5,12 +5,18 @@ from src_dir.backend import *
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
+        self.left = 300
+        self.top = 300
         self.width = 400
         self.height = 450
         self.init_widgets()
         self.init_layout()
+        self.init_events()
 
     def init_widgets(self):
+        # title and dimensions
+        self.setWindowTitle("Bookstore Management System")
+        self.setGeometry(self.left, self.top, self.width, self.height)
         # labels
         self.bookTitle = QtWidgets.QLabel("Book Title")
         self.bookTitle.setAlignment(QtCore.Qt.AlignCenter)
@@ -21,8 +27,8 @@ class MyWidget(QtWidgets.QWidget):
         self.bookISBN = QtWidgets.QLabel("ISBN")
         self.bookISBN.setAlignment(QtCore.Qt.AlignCenter)
         # entry fields
-        self.bookTitleEntry = QtWidgets.QLineEdit("Enter book Title")
-        self.bookAuthorEntry = QtWidgets.QLineEdit("Enter Author name")
+        self.bookTitleEntry = QtWidgets.QLineEdit("Enter Title")
+        self.bookAuthorEntry = QtWidgets.QLineEdit("Enter Author")
         self.bookPriceEntry = QtWidgets.QLineEdit("Enter Price")
         self.bookISBNEntry = QtWidgets.QLineEdit("Enter ISBN")
         # buttons
@@ -48,6 +54,15 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.bookAuthorEntry, 1, 1)
         self.layout.addWidget(self.bookPriceEntry, 2, 1)
         self.layout.addWidget(self.bookISBNEntry, 3, 1)
+        # listbox
+        self.layout.addWidget(self.listbox, 6, 0, 5, 5)
+        # buttons
+        self.layout.addWidget(self.buttonOne, 0, 2)
+        self.layout.addWidget(self.buttonTwo, 1, 2)
+        self.layout.addWidget(self.buttonThree, 2, 2)
+        self.layout.addWidget(self.buttonFour, 3, 2)
+        self.layout.addWidget(self.buttonFive, 4, 2)
+        self.layout.addWidget(self.buttonSix, 5, 2)
 
         self.setLayout(self.layout)
         return self
